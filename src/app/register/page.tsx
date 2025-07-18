45ca5a767833626b80ebf630a802b116c27e5ca0
ClickShop <clickshop.empresa@gmail.com>
2025-07-18 16:07:01 -0400

    Descripción del cambio

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,7 +2,7 @@ import { RegisterForm } from '@/components/auth/register-form'
 
 export default function RegisterPage() {
   return (
-    <div className="w-full max-w-md">
+    <div className="w-full max-w-md mx-auto pt-10">
       <RegisterForm />
     </div>
   )
