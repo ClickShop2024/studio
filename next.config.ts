d69c2fc3da24710f04bf75412daf0036530899bd
ClickShop <clickshop.empresa@gmail.com>
2025-07-18 14:32:36 -0400

    Activar modo export en Next.js para despliegue estático

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // ⬅️ Esta línea activa el modo estático
   typescript: {
     ignoreBuildErrors: true,
   },
